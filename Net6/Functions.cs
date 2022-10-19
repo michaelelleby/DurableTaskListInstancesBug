82159eba99bd7590353d24d6b8eb63331522bed5
Michael Elleby <michael@softwareahead.dk>
2022-10-19 11:35:33 +0200

    Implemented .NET 6 app

---
+++ Net6/Functions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+
+namespace Net6
+{
+    public class Functions
+    {
+        public async Task TimerStart([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer, [DurableClient] IDurableOrchestrationClient client)
+        {
+            await client.ListInstancesAsync(new OrchestrationStatusQueryCondition(), CancellationToken.None);
+        }
+    }
+}
---
+++ Net6/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Extensions.Hosting;
+
+namespace Net6
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var hostBuilder = new HostBuilder()
+                .ConfigureWebJobs(config =>
+                {
+                    config.AddAzureStorageCoreServices();
+                    config.AddTimers();
+                    config.AddDurableTask(options => { options.HubName = "MyTaskHub"; });
+                })
+                .UseConsoleLifetime();
+
+            var host = hostBuilder.Build();
+
+            using (host)
+            {
+                await host.RunAsync();
+            }
+        }
+    }
+}
